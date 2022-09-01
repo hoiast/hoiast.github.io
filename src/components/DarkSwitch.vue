2e6ab9fac20d06f4c4f00c42315245d0f9b64e04
hoiast <murilohoias@gmail.com>
2022-09-01 20:06:04 -0300

    Fix Dark Swtich
    Should always add 'dark' to root element (html). It was wrongly adding it to body on initialization.

--- src/components/DarkSwitch.vue
+++ src/components/DarkSwitch.vue
@@ -2,17 +2,18 @@
 import { ref, watch } from "vue";
 // Default to dark mode if the user has dark mode enabled.
 const dark = ref(window.matchMedia("(prefers-color-scheme: dark)").matches);
-if (dark.value) document.body.classList.add("dark");
-
-// Watch for changes in the user's preferred color scheme.
-watch(dark, (value) => {
+const changeDarkClassToRoot = (shouldAdd: boolean) => {
   const root = document.getElementsByTagName("html")[0];
-  if (value) {
+  if (shouldAdd) {
     root.classList.add("dark");
   } else {
     root.classList.remove("dark");
   }
-});
+};
+changeDarkClassToRoot(dark.value);
+
+// Watch for changes in the user's preferred color scheme.
+watch(dark, changeDarkClassToRoot);
 
 const toggleDark = () => {
   dark.value = !dark.value;
