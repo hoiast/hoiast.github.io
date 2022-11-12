8bf131fcd58200d5b32f987571d17f7f5228e912
Murilo Hoias Teixeira <murilohoias@gmail.com>
2022-11-12 12:24:34 -0300

    Add Footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
 import NavBar from "./components/NavBar/NavBar.vue";
+import Footer from "./components/FooterBar/FooterBar.vue";
 </script>
 
 <template>
   <NavBar />
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped></style>
---
+++ src/components/FooterBar/FooterBar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const year = new Date().getFullYear();
+const colorPattern = ["#65B947", "#27548E"];
+const lgColorString = `linear-gradient(${colorPattern.join(",")})`;
+</script>
+<template>
+  <footer
+    class="relative top-[10vh] py-6 bg-base transition-base color-base flex flex-col items-center"
+  >
+    <p>Murilo Hoias Teixeira - {{ year }}</p>
+    <p>
+      <a
+        href="https://github.com/hoiast/hoiast.github.io"
+        class="flex items-center gap-2 px-1 rounded-lg cursor-pointer text-white"
+        :style="`background-image: ${lgColorString}`"
+        aria-label="Portfolio GitHub repository"
+      >
+        <div class="i-carbon-logo-github"></div>
+        Portfolio Repository
+      </a>
+    </p>
+    <p>This portfolio is hosted with GitHub Pages and Actions</p>
+  </footer>
+</template>
+<style scoped></style>
